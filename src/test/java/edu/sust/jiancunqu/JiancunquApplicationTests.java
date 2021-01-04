f635cf72975093d9a01ec25a169087fe08ab07f7
SunnyGrocery <893210165@qq.com>
2021-01-04 14:44:17 +0800

    first commit

---
+++ src/test/java/edu/sust/jiancunqu/JiancunquApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sust.jiancunqu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiancunquApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
